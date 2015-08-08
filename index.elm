d233a9081e85e8a8a477fc103ad4ec7385c31f46
eeue56 <noah@astraldynamics.co.uk>
2015-08-08 07:22:38 +0100

    added init board

---
+++ index.elm
@@ -0,0 +1,62 @@
+import Html exposing (div, button, text)
+import StartApp
+
+import String exposing (dropLeft)
+
+import Color exposing (..)
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
+
+
+main =
+  StartApp.start { model = board, view = drawBoard', update = update }
+
+type alias Position = {x: Float, y: Float}
+type alias Piece = { img : String, pos: Position}
+type alias Board = { pieces: List (Piece), width: Float, height: Float }
+
+model = "Hello"
+board : Board
+board = { 
+    pieces = makePieces 5 5,
+    width = 500,
+    height = 500
+  }
+
+pieceSize = 50
+
+genBoard x y = if
+  | x < 0 || y < 0 -> []
+  | x == 0 -> List.map (\y -> (0, y)) [1..y]
+  | y == 0 -> List.map (\x -> (x, 0)) [1..x]
+  | otherwise -> (genBoard x 0) ++ (genBoard 0 y) ++ List.map2 (\x y -> (x, y)) [0..x] [0..y] 
+
+makePiece : Float -> Float -> Piece
+makePiece x y = Piece "" {x = x, y = y}
+
+makePieces : Float -> Float -> List Piece
+makePieces x y = if
+  | x < 0 || y < 0 -> []
+  | otherwise -> List.map (\(x, y) -> makePiece x y) <| genBoard x y
+
+
+drawBoard address board = 
+  div [] <| List.map (\piece -> button [] [Html.text <| toString [piece.pos.x, piece.pos.y]]) <| board.pieces
+
+genRect board piece =
+  let 
+    startWidth = board.width / 2 - pieceSize
+    startHeight = board.height / 2 - pieceSize
+  in 
+    rect pieceSize pieceSize
+      |> filled (rgba 111 111 111 0.6)
+      |> move (toFloat pieceSize * piece.pos.x - startWidth, toFloat pieceSize * piece.pos.y - startHeight)
+
+
+drawBoard' address board =
+  Html.fromElement <| 
+    collage (round board.width) (round board.height) <| List.map (genRect board) board.pieces
+
+type Action = Increment | Decrement | Reset
+
+update action model = model
