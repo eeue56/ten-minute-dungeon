ca98487978c3815775b3d2f289b2f51285fb2e2b
eeue56 <noah@astraldynamics.co.uk>
2015-08-10 07:05:26 +0100

    remove startapp and go full custom

--- index.elm
+++ index.elm
@@ -4,9 +4,6 @@ import String exposing (dropLeft)
 import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
-import Set
-import Dict
-import Keyboard
 
 import Html.Events exposing (onKeyDown, onClick, onKeyPress)
 
@@ -15,31 +12,20 @@ import Coords exposing (..)
 import Board exposing (..)
 import Drawing exposing (..)
 import Signal exposing (..)
+import Input exposing (..)
 
-type alias App model action =
-    { model : model
-    , view : Signal.Address action -> model -> Html.Html
-    , update : action -> model -> model
-    }
+input = Input <~ playerDirection
 
-start : App model action -> Signal Html.Html
-start app =
-  let
-    actions =
-      Signal.mailbox Nothing
 
-    address =
-      Signal.forwardTo actions.address Just
+model =
+  Signal.foldp
+    update
+    board
+    input
 
-    model =
-      Signal.foldp
-        (\(Just action) model -> app.update action model)
-        app.model
-        actions.signal
-  in
-    (app.view address) <~ (model) 
+main = drawBoard <~ model
 
-main = start { model = board, view = drawBoard, update = update }
+
 
 board : Board
 board = {
@@ -51,28 +37,20 @@ board = {
   }
 
 
-type Action = None | Dogs Int
 
-
-makePlayer : Int -> Int -> WorldObject
-makePlayer x y = Player {pos = {x=x, y=y}, img = "graphics/player.jpg"} 
-
-
-makePieces : Int -> Int -> List WorldObject
+makePieces : Int -> Int -> List Piece
 makePieces x y = if
   | x < 0 || y < 0 -> []
   | otherwise -> List.map (\(x, y) -> makePiece (x) (y) ) <| genBoard x y
 
-genRect = drawWorldObject
-
 boardCollage board = collage (round board.width) (round board.height)
 
-drawBoard : Signal.Address Action -> Board -> Html.Html
-drawBoard address board =
-  div [onClick address <| Dogs 87]
+
+drawBoard board =
+  div []
   [
     Html.fromElement <| 
-      layers [(boardCollage board <| List.map (genRect board) board.pieces), (boardCollage board <| [genRect board board.player])]
+      layers [(boardCollage board <| List.map (drawPiece board) board.pieces), (boardCollage board <| [drawPlayer board board.player])]
     ]
 
 drawBoardCoords address board = 
@@ -80,12 +58,3 @@ drawBoardCoords address board =
 
 updateBoard : Board -> Bool -> Board
 updateBoard board isDown = if isDown then { board - player | player = makePlayer 3 3 } else board
-
-update : Action -> Board -> Board
-update action board = 
-  case action of 
-    None -> board
-    Dogs key -> if key == 87 then updateBoard board True else board
-
-pressed : Signal Bool
-pressed = Keyboard.isDown 87
